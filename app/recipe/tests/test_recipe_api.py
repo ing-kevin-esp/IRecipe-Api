60686c87784adfca21c97777b4dd23d681c74e88
ing-kevin-esp <111006221+ing-kevin-esp@users.noreply.github.com>
2022-12-14 16:16:12 -0600

    add detail recipe get and create

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -11,9 +11,16 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.models import Recipe
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import (
+    RecipeSerializer,
+    RecipeDetailSerializer,
+)
 
 RECIPES_URL = reverse('recipe:recipe-list')
+
+
+def detail_url(recipe_id):
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 
 def create_recipe(user, **params):
@@ -31,7 +38,7 @@ def create_recipe(user, **params):
     return recipe
 
 
-class t(TestCase):
+class PublicRecipeAPITests(TestCase):
     """Test unauthorize api requests """
 
     def setUp(self):
@@ -52,8 +59,7 @@ class PrivateRecipeAPITests(TestCase):
             email='user@example.com',
             password='testpass123'
         )
-        self.client.force_authenticate(self.user
-                                       )
+        self.client.force_authenticate(self.user)
 
     def test_retrieve_recipes(self):
         """Test retrieving a list of recipes"""
@@ -84,3 +90,32 @@ class PrivateRecipeAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        """Test get recipe detail"""
+        recipe = create_recipe(user=self.user)
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_recipe_by_post(self):
+        """Test creating a recipe by post method"""
+        payload = {
+            'title': 'Sample recipe title by post',
+            'time_minutes': 25,
+            'price': Decimal('5.25'),
+            'description': 'Sample description',
+            'link': 'http://example.com/recipe.pdf'
+        }
+        res = self.client.post(RECIPES_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+
+        for k, v in payload.items():
+            self.assertEqual(getattr(recipe, k), v)
+
+        self.assertEqual(recipe.user, self.user)
